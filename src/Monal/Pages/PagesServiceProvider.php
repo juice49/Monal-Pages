42dd73f9437564f64edfd4565bece77e67b8a847
Arran Jacques <arranjacques@gmail.com>
2014-06-06 13:34:47 +0100

    Adds installer to the packages service provider.

--- src/Monal/Pages/PagesServiceProvider.php
+++ src/Monal/Pages/PagesServiceProvider.php
@@ -2,8 +2,10 @@
 namespace Monal\Pages;
 
 use Illuminate\Support\ServiceProvider;
+use Monal\MonalPackageServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
 
-class PagesServiceProvider extends ServiceProvider {
+class PagesServiceProvider extends ServiceProvider implements MonalPackageServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
@@ -11,6 +13,70 @@ class PagesServiceProvider extends ServiceProvider {
 	 * @var		Boolean
 	 */
 	protected $defer = false;
+
+	/**
+	 * Return the package's namespace.
+	 *
+	 * @return	String
+	 */
+	public function packageNamespace()
+	{
+		return 'monal/pages';
+	}
+
+	/**
+	 * Return the package's details.
+	 *
+	 * @return	Array
+	 */
+	public function packageDetails()
+	{
+		return array(
+			'name' => 'Pages',
+			'author' => 'Arran Jacques',
+			'version' => '0.9.0',
+		);
+	}
+
+	/**
+	 * Install the package.
+	 *
+	 * @return	Boolean
+	 */
+	public function install()
+	{
+		mkdir(public_path() . '/packages/pages');
+		\Resource::cloneDirecotry(__DIR__ . '/../../../public', public_path() . '/packages/pages');
+		\Schema::create(
+			'pages',
+			function(Blueprint $table) {
+				$table->increments('id');
+				$table->integer('page_type')->nullable();
+				$table->string('name', 255)->nullable();
+				$table->integer('parent')->nullable();
+				$table->string('slug', 255)->nullable();
+				$table->text('url')->nullable();
+				$table->string('title', 255)->nullable();
+				$table->text('description')->nullable();
+				$table->text('keywords')->nullable();
+				$table->tinyInteger('is_home')->nullable();
+				$table->integer('order')->nullable();
+				$table->timestamps();
+			}
+		);
+		\Schema::create(
+			'page_types',
+			function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('name', 255)->nullable();
+				$table->string('table_prefix', 255)->nullable();
+				$table->string('template', 255)->nullable();
+				$table->text('data_set_templates')->nullable();
+				$table->timestamps();
+			}
+		);
+		return true;
+	}
 
 	/**
 	 * Bootstrap the application events.
@@ -88,35 +154,26 @@ class PagesServiceProvider extends ServiceProvider {
 		);
 
 		// Register Facades
-		$this->app['pageshelper'] = $this->app->share(
-			function ($app) {
+		$this->app['pageshelper'] = $this->app->share(function ($app) {
 				return new \Monal\Pages\Libraries\PagesHelper;
-			}
-		);
-		$this->app->booting(
-			function () {
+		});
+		$this->app->booting(function () {
 			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
 			$loader->alias('PagesHelper', 'Monal\Pages\Facades\PagesHelper');
 		});
 
-		$this->app['pagesrepository'] = $this->app->share(
-			function ($app) {
+		$this->app['pagesrepository'] = $this->app->share(function ($app) {
 				return \App::make('Monal\Pages\Repositories\PagesRepository');
-			}
-		);
-		$this->app->booting(
-			function () {
+		});
+		$this->app->booting(function () {
 			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
 			$loader->alias('PagesRepository', 'Monal\Pages\Facades\PagesRepository');
 		});
 
-		$this->app['pagetypesrepository'] = $this->app->share(
-			function ($app) {
+		$this->app['pagetypesrepository'] = $this->app->share(function ($app) {
 				return \App::make('Monal\Pages\Repositories\PageTypesRepository');
-			}
-		);
-		$this->app->booting(
-			function () {
+		});
+		$this->app->booting(function () {
 			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
 			$loader->alias('PageTypesRepository', 'Monal\Pages\Facades\PageTypesRepository');
 		});
